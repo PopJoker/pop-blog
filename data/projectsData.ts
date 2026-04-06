a534fedf80fb7390f835adb0fdb55bb48407c337
PopJoker <52096871+PopJoker@users.noreply.github.com>
2026-04-06 21:05:17 +0800

    04/06_15

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -15,12 +15,10 @@ const projectsData: Project[] = [
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Meney',
+    description: `待更新`,
+    imgSrc: '/static/images/Meney_Slogan.png',
+    href: '/blog/meney',
   },
 ]
 
