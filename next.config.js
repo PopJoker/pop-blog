f2ac635befaf727936a85a5c318c39567d3f0de4
PopJoker <52096871+PopJoker@users.noreply.github.com>
2026-04-16 21:14:30 +0800

    fix lint format

--- next.config.js
+++ next.config.js
@@ -1,58 +1,84 @@
-const { withContentlayer } = require('next-contentlayer')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const nextConfig = {
-  output: process.env.EXPORT ? 'export' : undefined,
-  basePath: process.env.BASE_PATH || undefined,
-  reactStrictMode: true,
-  trailingSlash: true,
-
-  turbopack: {
-    root: process.cwd(),
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
+module.exports = () => {
+  return {
+    output: process.env.EXPORT ? 'export' : undefined,
+    basePath: process.env.BASE_PATH || undefined,
+    reactStrictMode: true,
+    trailingSlash: true,
+    turbopack: {
+      root: process.cwd(),
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
       },
     },
-  },
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'picsum.photos',
+        },
+      ],
+      unoptimized: process.env.UNOPTIMIZED ? true : undefined,
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+            {
+              key: 'Content-Security-Policy',
+              value:
+                "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;",
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+            {
+              key: 'X-DNS-Prefetch-Control',
+              value: 'on',
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=31536000; includeSubDomains',
+            },
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+            {
+              key: 'Permissions-Policy',
+              value: 'camera=(), microphone=(), geolocation=()',
+            },
+          ],
+        },
+      ]
+    },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
 
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
-    unoptimized: !!process.env.UNOPTIMIZED,
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;",
-          },
-        ],
-      },
-    ]
-  },
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
+      return config
+    },
+  }
 }
-
-module.exports = withContentlayer(withBundleAnalyzer(nextConfig))
