c9424ce22c341e048f3d753c6464d2ffa2d33556
PopJoker <52096871+PopJoker@users.noreply.github.com>
2026-04-16 20:56:32 +0800

    fix lint format

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
+const { withContentlayer } = require('next-contentlayer')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = () => {
-  return {
+module.exports = withBundleAnalyzer(
+  withContentlayer({
     output: process.env.EXPORT ? 'export' : undefined,
     basePath: process.env.BASE_PATH || undefined,
     reactStrictMode: true,
     trailingSlash: true,
+
     turbopack: {
       root: process.cwd(),
       rules: {
@@ -17,7 +19,9 @@ module.exports = () => {
         },
       },
     },
+
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+
     images: {
       remotePatterns: [
         {
@@ -27,43 +31,37 @@ module.exports = () => {
       ],
       unoptimized: process.env.UNOPTIMIZED ? true : undefined,
     },
+
     async headers() {
       return [
         {
           source: '/(.*)',
           headers: [
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
             {
               key: 'Content-Security-Policy',
               value:
                 "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;",
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
             {
               key: 'Referrer-Policy',
               value: 'strict-origin-when-cross-origin',
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
             {
               key: 'X-Frame-Options',
               value: 'DENY',
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
             {
               key: 'X-Content-Type-Options',
               value: 'nosniff',
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
             {
               key: 'X-DNS-Prefetch-Control',
               value: 'on',
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
             {
               key: 'Strict-Transport-Security',
               value: 'max-age=31536000; includeSubDomains',
             },
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
             {
               key: 'Permissions-Policy',
               value: 'camera=(), microphone=(), geolocation=()',
@@ -72,7 +70,8 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config, options) => {
+
+    webpack: (config) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -80,5 +79,5 @@ module.exports = () => {
 
       return config
     },
-  }
-}
+  })
+)
