ab065dc99bfd983718193af04919ab5dfcc8fb52
PopJoker <52096871+PopJoker@users.noreply.github.com>
2026-04-19 14:58:34 +0800

    fix lint format

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,6 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = '/pop-blog'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`${basePath}${src}`} {...rest} />
 
 export default Image
